390c5fbec649c07708750005cc1557e89cab191c
Sanjam <sanjamkr4@gmail.com>
2017-07-20 23:35:46 +0530

    Location

--- dtp/settings.py
+++ dtp/settings.py
@@ -145,4 +145,35 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6Lfd8iYUAAAAAGCC-LXkYded7hMrhLB2KJ65XDsD'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
-'''GDAL_LIBRARY_PATH = 'C:\Program Files (x86)\GDAL\projlib' '''
+
+LOCATION_FIELD_PATH = STATIC_URL + 'location_field'
+
+LOCATION_FIELD = {
+    'map.provider': 'google',
+    'map.zoom': 13,
+
+    'search.provider': 'google',
+    'search.suffix': '',
+
+    # Google
+    'provider.google.api': '//maps.google.com/maps/api/js',
+    'provider.google.api_key': 'AIzaSyCe1Uuv1gE8KLBlK7zrjxDXd_ruSgrtNKE',
+    'provider.google.map_type': 'ROADMAP',
+
+    # Mapbox
+    'provider.mapbox.access_token': '',
+    'provider.mapbox.max_zoom': 18,
+    'provider.mapbox.id': 'mapbox.streets',
+
+    # OpenStreetMap
+    'provider.openstreetmap.max_zoom': 18,
+
+    # misc
+    'resources.root_path': LOCATION_FIELD_PATH,
+    'resources.media': {
+        'js': [
+            LOCATION_FIELD_PATH + '/js/jquery.livequery.js',
+            LOCATION_FIELD_PATH + '/js/form.js',
+        ],
+    },
+}
